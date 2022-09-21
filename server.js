de640090bef67e05e15b843e1816d71ee2eba2c9
Patrick <patasax22@yahoo.com>
2022-09-21 16:17:09 -0400

    progress with some issues

--- server.js
+++ server.js
@@ -1,41 +1,46 @@
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const passport = require('passport')
 
-const users = []
+const initializePassport = require('./passport-config');
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email)
+)
 
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
+const users = [];
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'Kyle' })
-})
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/login', (req, res) => {
-    res.render('login.ejs')
-})
+app.get("/", (req, res) => {
+	res.render("index.ejs", { name: "Kyle" });
+});
 
-app.post('/login', (req, res) => {
-    
-})
+app.get("/login", (req, res) => {
+	res.render("login.ejs");
+});
 
-app.get('/register', (req, res) => {
-    res.render('register.ejs')
-})
+app.post("/login", (req, res) => {});
 
-app.post('/register', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login')
-    } catch {
-        res.redirect('/register')
-    }
-})
+app.get("/register", (req, res) => {
+	res.render("register.ejs");
+});
 
-app.listen(3000)
+app.post("/register", async (req, res) => {
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		users.push({
+			id: Date.now().toString(),
+			name: req.body.name,
+			email: req.body.email,
+			password: hashedPassword,
+		});
+		res.redirect("/login");
+	} catch {
+		res.redirect("/register");
+	}
+});
+
+app.listen(3000);
